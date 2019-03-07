838aea2b8dac443d5965718715709adb5b538865
Qais Patankar <qaisjp@gmail.com>
2019-03-07 04:54:27 +0000

    Fix remainder pages for init

--- buddy.cpp
+++ buddy.cpp
@@ -498,26 +498,25 @@ public:
 	{
 		mm_log.messagef(LogLevel::DEBUG, "Buddy Allocator Initialising pd=%p, nr=0x%lx", page_descriptors, nr_page_descriptors);
 		
-		// TODO: Initialise the free area linked list for the maximum order
+		// Initialise the free area linked list for the maximum order
 		// to initialise the allocation algorithm.
-		auto largest_block_size = pages_per_block(MAX_ORDER);
-		auto remaining_pages = nr_page_descriptors % largest_block_size;
-		auto block_count = (nr_page_descriptors - remaining_pages) / largest_block_size;
+		auto order = MAX_ORDER;
+		uint64_t remaining_pages = nr_page_descriptors;
 
-		auto last_block = page_descriptors + (block_count * largest_block_size);
+		do {
+			auto block_size = pages_per_block(order);
+			auto surplus_pages = remaining_pages % block_size;
+			auto block_count = (remaining_pages - surplus_pages) / block_size;
+			auto last_block = page_descriptors + (block_count * block_size);
 
-		debugf("INIT: pages per block: %d, blocks to allocate: %d", largest_block_size, block_count);
+			while (page_descriptors < last_block) {
+				insert_block(page_descriptors, order);
+				page_descriptors += block_size;
+				remaining_pages -= block_size;
+			}
 
-		for (auto block = page_descriptors; block <= last_block; block += largest_block_size) {
-			insert_block(block, MAX_ORDER);
-		}
-
-		debugf("INIT: done inserting pages.")
-
-		// todo
-		if (remaining_pages != 0) {
-			assert(false && "init function - remaining_pages is 0 - NOT IMPLEMENTED");
-		}
+			order--;
+		} while (remaining_pages > 0);
 
 		debugf("INIT: done initialising buddy algorithm")
 		return true;
